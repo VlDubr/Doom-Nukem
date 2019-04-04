4142ed29b1f624efd5de37a628c83f2289c1d66c
Gormlaith Daniel <gdaniel@mi-i6.21-school.ru>
2019-04-04 13:28:04 +0300

    Edit

---
+++ includes/doom.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   doom.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdaniel <gdaniel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/02 19:40:29 by gdaniel           #+#    #+#             */
+/*   Updated: 2019/04/02 19:45:16 by gdaniel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOOM_H
+# define DOOM_H
+
+typedef struct	s_mouse
+{
+
+}				t_mouse;
+
+typedef struct	s_input
+{
+
+}				t_input;
+
+
+typedef struct	s_doom
+{
+}				t_doom;
+
+
+void	updateevent(void);
+void	update(void);
+void	draw(void);
+
+#endif
